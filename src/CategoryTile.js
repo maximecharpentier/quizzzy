8b2cb90207402fc73ab930ed2a6f71e8cfe1b6d7
blyndusk <alexandredelaloydev@gmail.com>
2018-12-30 18:20:59 +0100

    👌 CategoryTile usable
     #4

--- src/CategoryTile.js
+++ src/CategoryTile.js
@@ -7,41 +7,20 @@ const SVGs = [brandSVG, capitalSVG, moviesSVG];
 class CategoryTile extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            category: 0,
-            hidden: false
-        }
+        this.state = {category: 0}
     }
-    componentDidMount = () => {
-        this.categoryID = setInterval(() => this.getPoints(), 200);
-    }
-    componentWillUnmount= () => {
-        // use intervalId from the state to clear the interval
-        clearInterval(this.categoryID);
-    }
-    getPoints = () => {
-        // setState method is used to update the state
-        return this.setState({
-            category: localStorage.getItem('category')
-        })
-    }
-    onClick = () => {
-        this.setState({hidden: true})
-    }
+    componentDidMount = () => this.categoryID = setInterval(() => this.getPoints(), 200);
+    componentWillUnmount = () => clearInterval(this.categoryID);
+    getPoints = () => this.setState({category: localStorage.getItem('category')});
     render() {
         let CategoryName = this.props.data.categories[this.state.category].name;
         let CategoryUrl = SVGs[this.state.category];
         return (
-            <section className={this.state.hidden ? 'tile tile--category hidden' : 'tile tile--category showed'} onClick={this.onClick}>
+            <section className={this.props.style} onClick={this.props.click}>
                 <h2 className="tile__title">{this.props.data.category.title.welcome}&nbsp;<span className="bold">{this.props.data.category.title.name}</span></h2>
                 <p className="tile__info">{this.props.data.category.desc}</p>
                 <img className="tile__image" src={CategoryUrl} alt=""/>
                 <h3 className="tile__category">{CategoryName}</h3>
-                <div className="dots">
-                    <div className="dots__item"></div>
-                    <div className="dots__item"></div>
-                    <div className="dots__item"></div>
-                </div>
                 <p className="tile__instruction">{this.props.data.category.footer}</p>
             </section>
         )
