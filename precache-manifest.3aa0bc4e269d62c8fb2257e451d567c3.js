bcc23f69e052aeb211be1b2eabd484f3e221423c
blyndusk <alexandredelaloydev@gmail.com>
2019-01-04 16:53:01 +0100

    Updates

---
+++ precache-manifest.3aa0bc4e269d62c8fb2257e451d567c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6da3589fe697aa95b52d5500ab9284f",
+    "url": "/quizzzy/static/media/brand.b6da3589.svg"
+  },
+  {
+    "revision": "7face1478fe65c4abeb6",
+    "url": "/quizzzy/static/css/main.b3871a6f.chunk.css"
+  },
+  {
+    "revision": "2e8aeaab1227158c8427",
+    "url": "/quizzzy/static/js/1.2e8aeaab.chunk.js"
+  },
+  {
+    "revision": "88ede40da4b1b8e3a379",
+    "url": "/quizzzy/static/js/runtime~main.88ede40d.js"
+  },
+  {
+    "revision": "185884df5b948202df8c8a68a453ff9d",
+    "url": "/quizzzy/static/media/false.185884df.svg"
+  },
+  {
+    "revision": "d5bf6ebcc4ba1286630cd7414a132fb7",
+    "url": "/quizzzy/static/media/globe.d5bf6ebc.svg"
+  },
+  {
+    "revision": "7face1478fe65c4abeb6",
+    "url": "/quizzzy/static/js/main.7face147.chunk.js"
+  },
+  {
+    "revision": "8149793e4f9c7681419fb8b3df82a774",
+    "url": "/quizzzy/static/media/capital.8149793e.svg"
+  },
+  {
+    "revision": "08c5768c2cc95050aee98da7a289f198",
+    "url": "/quizzzy/static/media/movies.08c5768c.svg"
+  },
+  {
+    "revision": "f821db6c393c0e302a2d7876f74df25f",
+    "url": "/quizzzy/static/media/correct.f821db6c.svg"
+  },
+  {
+    "revision": "b1c117ac202f72a8131c9548797508d0",
+    "url": "/quizzzy/static/media/winner.b1c117ac.svg"
+  },
+  {
+    "revision": "f9b5a40e768de12ade132a753debbf47",
+    "url": "/quizzzy/static/media/looser.f9b5a40e.svg"
+  },
+  {
+    "revision": "054238740d1a7de934a3231317454bbb",
+    "url": "/quizzzy/index.html"
+  }
+];
