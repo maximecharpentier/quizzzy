8f30a33511bbd10fc0123ec3a1053fe2d5ff3ede
blyndusk <alexandredelaloydev@gmail.com>
2019-04-18 11:53:03 +0200

    :card_file_box: move data

---
+++ src/data/data.js
@@ -0,0 +1,101 @@
+const data = {
+    api: {
+        "id":78,
+        "title":"world capitals",
+        "clues_count":140,
+        "clues": [
+            {
+                "id": 481, "answer": "San Jose", "question": "Costa Rican capital people might know the way to", "value": 100, "airdate": "1984-09-25T12:00:00.000Z", "category_id": 78, "game_id": null, "invalid_count": null
+                }
+                ,
+                {
+                    "id": 487, "answer": "Tokyo", "question": "It's divided into the Kanda, Marunoucki, Asakusa \u0026 Ginza districts", "value": 200, "airdate": "1984-09-25T12:00:00.000Z", "category_id": 78, "game_id": null, "invalid_count": null
+                }
+                ,
+                {
+                    "id": 493, "answer": "Brussels", "question": "Home to NATO \u0026 little green sprouts", "value": 300, "airdate": "1984-09-25T12:00:00.000Z", "category_id": 78, "game_id": null, "invalid_count": null
+                },
+                {
+                    "id": 481, "answer": "San Jose", "question": "Costa Rican capital people might know the way to", "value": 100, "airdate": "1984-09-25T12:00:00.000Z", "category_id": 78, "game_id": null, "invalid_count": null
+                }
+                ,
+                {
+                    "id": 487, "answer": "Tokyo", "question": "It's divided into the Kanda, Marunoucki, Asakusa \u0026 Ginza districts", "value": 200, "airdate": "1984-09-25T12:00:00.000Z", "category_id": 78, "game_id": null, "invalid_count": null
+                }
+                ,
+                {
+                    "id": 493, "answer": "Brussels", "question": "Home to NATO \u0026 little green sprouts", "value": 300, "airdate": "1984-09-25T12:00:00.000Z", "category_id": 78, "game_id": null, "invalid_count": null
+                },
+                {
+                    "id": 481, "answer": "San Jose", "question": "Costa Rican capital people might know the way to", "value": 100, "airdate": "1984-09-25T12:00:00.000Z", "category_id": 78, "game_id": null, "invalid_count": null
+                }
+                ,
+                {
+                    "id": 487, "answer": "Tokyo", "question": "It's divided into the Kanda, Marunoucki, Asakusa \u0026 Ginza districts", "value": 200, "airdate": "1984-09-25T12:00:00.000Z", "category_id": 78, "game_id": null, "invalid_count": null
+                }
+                ,
+                {
+                    "id": 493, "answer": "Brussels", "question": "Home to NATO \u0026 little green sprouts", "value": 300, "airdate": "1984-09-25T12:00:00.000Z", "category_id": 78, "game_id": null, "invalid_count": null
+                },
+                {
+                    "id": 481, "answer": "San Jose", "question": "Costa Rican capital people might know the way to", "value": 100, "airdate": "1984-09-25T12:00:00.000Z", "category_id": 78, "game_id": null, "invalid_count": null
+                }
+                ,
+                {
+                    "id": 487, "answer": "Tokyo", "question": "It's divided into the Kanda, Marunoucki, Asakusa \u0026 Ginza districts", "value": 200, "airdate": "1984-09-25T12:00:00.000Z", "category_id": 78, "game_id": null, "invalid_count": null
+                }
+                ,
+                {
+                    "id": 493, "answer": "Brussels", "question": "Home to NATO \u0026 little green sprouts", "value": 300, "airdate": "1984-09-25T12:00:00.000Z", "category_id": 78, "game_id": null, "invalid_count": null
+                }
+        ]
+    },
+    header: {
+        title: 'Quizzzy'
+    },
+    footer: {
+        author: 'Hetic W2',
+        service: {
+            by: 'Powered by',
+            name: 'jService',
+        }
+    },
+    error: {
+        title: 'Oops !',
+        content: 'It appears that resources couldn\'t be loaded properly, please :',
+        cta: 'Try again'
+    },
+    categories: [
+        {
+            name: 'DEFAULT_CATEGORY',
+            url: 'http://jservice.io/api/category?id=78',
+            index: 0
+        },
+        {
+            name: 'DEFAULT_CATEGORY',
+            url: 'http://jservice.io/api/category?id=2537',
+            index: 1
+        },
+        {
+            name: 'DEFAULT_CATEGORY',
+            url: 'http://jservice.io/api/category?id=2289',
+            index: 2
+        }
+    ],
+    category: {
+        title: {
+            welcome: 'Welcome on',
+            name: 'Quizzzy'
+        },
+        desc: 'Click on the tile to begin'
+    },
+    result: {
+        win: 'Your\'re a winner !',
+        mid: 'You can do better !',
+        loose: 'You lost !',
+        another: 'Come on !',
+        again: 'Let\'s see how good you are !'
+    }
+}
+
+export default data;
